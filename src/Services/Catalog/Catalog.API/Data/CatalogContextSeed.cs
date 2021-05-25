6236a15479769a0c91574b29712e78bca3353c73
festypat <festypat9@gmail.com>
2021-05-25 22:24:30 +0100

    Updated catalog api

---
+++ src/Services/Catalog/Catalog.API/Data/CatalogContextSeed.cs
@@ -0,0 +1,47 @@
+﻿using Catalog.API.Entities;
+using MongoDB.Driver;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Catalog.API.Data
+{
+    public class CatalogContextSeed
+    {
+        public static void SeedData(IMongoCollection<Product> productCollection)
+        {
+            bool existProduct = productCollection.Find(p => true).Any();
+
+            if (!existProduct)
+                productCollection.InsertManyAsync(GetPreconfiguredProducts());
+        }
+
+        private static IEnumerable<Product> GetPreconfiguredProducts()
+        {
+            return new List<Product>()
+            {
+                new Product()
+                {
+                    Id = Guid.NewGuid().ToString("N"),
+                    Name = "Samsung",
+                    Summary = "Test",
+                    Description = "Mongo test",
+                    ImageFile = "product1.png",
+                    Price = 873.00M,
+                    Category = "Phones"
+                },
+                new Product()
+                {
+                    Id = Guid.NewGuid().ToString("N"),
+                    Name = "Chair",
+                    Summary = "Chair",
+                    Description = "Chair test",
+                    ImageFile = "product-2.png",
+                    Price = 324,
+                    Category = "Furniture"
+                }
+            };
+        }
+    }
+}
