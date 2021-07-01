878d6e64a0aaad62297732d7a6f0f14779ec8906
festypat <festypat9@gmail.com>
2021-07-01 22:54:25 +0100

    Created docker for discount api

--- src/Services/Discount/Discount.API/Repositories/DiscountRepository.cs
+++ src/Services/Discount/Discount.API/Repositories/DiscountRepository.cs
@@ -38,7 +38,7 @@ namespace Discount.API.Repositories
 
             var affected =
                 await connection.ExecuteAsync
-                ("INSERT INTO Coupon (ProductName, Description, Amount) VALUES (@productName, @Description @amount)",
+                ("INSERT INTO Coupon (ProductName, Description, Amount) VALUES (@productName, @Description, @amount)",
                 new { ProductName = coupon.ProductName, Description = coupon.Description, Amount = coupon.Amount });
 
             if (affected == 0)
